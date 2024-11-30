d47d904baceb2aa0ae56dcb4b976136b6fd09caf
zozo <zoe.guilbert.20@gmail.com>
2024-11-30 21:34:16 +0100

    columns named model_id

--- heatmap_correlation.py
+++ heatmap_correlation.py
@@ -3,12 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Charger les résultats de corrélation depuis un fichier CSV
-file_path = "/home/zozo/Bureau/Coarse_grain_RMSD/coarse-grained-RMSD/data/SCORES_test/pearson_results.csv"  # Remplacez par le chemin vers votre fichier
+file_path = "/home/zozo/Bureau/Coarse_grain_RMSD/P_C1_O3_SCORES_merged_nan/pearson_results.csv"  # Remplacez par le chemin vers votre fichier
 correlation_data = pd.read_csv(file_path)
 
 # Réorganiser les données pour une heatmap
 # On exclut la colonne "RNA" pour travailler sur les valeurs de corrélation
-correlation_matrix = correlation_data.set_index("RNA")
+correlation_matrix = correlation_data.set_index("model_id")
 
 # Créer une heatmap
 plt.figure(figsize=(8, 6))
